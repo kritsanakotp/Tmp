b15d3fe4355b6c7a9d80ccf7cd09e97f294f5620
kritsana kotprom <kritsana.kotp@msu.ac.th>
2018-08-07 14:43:12 +0700

    insert input

---
+++ Tmp/Input.swift
@@ -0,0 +1,13 @@
+//
+//  Input.swift
+//  Tmp
+//
+//  Created by iclassroom on 7/8/2561 BE.
+//  Copyright © 2561 iclassroom. All rights reserved.
+//
+
+import UIKit
+
+class Input: NSObject {
+
+}
